5d6d1f7578dbe1c47cb2268f1ee7dbee71853401
felipf <6341@holbertonstudents.com>
2023-06-04 12:17:59 -0700

    task8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def uppercase(string):
+    for char in string:
+        uppercase_char = chr(ord(char) - 32) if 97 <= ord(char) <= 122 else char
+        print(uppercase_char, end="")
+    print()
