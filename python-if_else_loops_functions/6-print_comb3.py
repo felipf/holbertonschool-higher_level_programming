5d1f0b9753bbbec2172320996165081b36980601
felipf <6341@holbertonstudents.com>
2023-06-04 12:08:00 -0700

    task fixed

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for tens_d in range(0, 10):
     for ones_d in range(tens_d + 1, 10):
-        print("{:d}{:d}".format(tens_d % 10, ones_d % 10), end=', ')
+        print("{:d}{:d}".format(tens_d % 10, ones_d % 10), end='')
+        if tens_d < 8 and ones_d <= 9:
+            print(', ', end='')
 print('')
