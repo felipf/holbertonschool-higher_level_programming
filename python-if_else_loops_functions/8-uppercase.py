cef12d80553a85cc970d14cff3d5bf50f82a29f6
felipf <6341@holbertonstudents.com>
2023-06-04 12:37:20 -0700

    string vacio añadido

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
+    new = " "
     for char in str:
-        if ord(char) >= ord('a') and ord(char) <= ord('z'):
-            char = chr(ord(char) - 32)
-            print("{}".format(char), end='')
-    print('')
+        if ord('a') <= ord(char) <= ord('z'):
+            char = ord(char) - 32
+            new += chr(char)
+        else:
+            new += char
+            print("{}".format(new))
