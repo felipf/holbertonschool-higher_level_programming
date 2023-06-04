4759f42e5c716f403975d22132f4d0db8d89eed0
felipf <6341@holbertonstudents.com>
2023-06-04 12:59:21 -0700

    final task fixed

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,7 +5,7 @@ def fizzbuzz():
             print("FizzBuzz", end=" ")
         elif i % 3 == 0:
             print("Fizz", end=" ")
-        elif:
+        elif i % 5 == 0:
             print("Buzz", end=" ")
         else:
             print(":{d}".format(i), end=" ")
