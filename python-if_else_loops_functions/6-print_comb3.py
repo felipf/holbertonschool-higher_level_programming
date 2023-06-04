09c80ba96cdb6286a7b92ea087ecaffa00e57f99
felipf <6341@holbertonstudents.com>
2023-06-04 12:06:25 -0700

    task fixed

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for tens_digit in range(0, 10):
-    for ones_digit in range(tens_digit + 1, 10):
-        print(f"{tens_digit}{ones_digit:0}", end=", ")
-print()
+for tens_d in range(0, 10):
+    for ones_d in range(tens_d + 1, 10):
+        print("{:d}{:d}".format(tens_d % 10, ones_d % 10), end=', ')
+print('')
