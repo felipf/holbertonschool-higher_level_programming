e9367614e46e1bf17ad226719524ed9f5f4ae7f6
felipf <6341@holbertonstudents.com>
2023-06-04 11:41:56 -0700

    task3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z') + 1):
-    if chr(letter) not in ('q', 'e'):
-        print(chr(letter), end='')
+for letter in range(97, 123):
+    if alph == 101 or alph == 113:
+        continue
+    print("{:c}".format(alph), end='')
