2a94d4054a62569e340e0c47334ffe7d6fdeb832
felipf <6341@holbertonstudents.com>
2023-06-14 07:19:16 -0700

    task4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,42 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+4-square.py
+author: felipf
+"""
+
+
+class Square:
+    """
+    Square defined
+    """
+
+    def __init__(self, size=0):
+        """
+        size predefined
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        retrieve size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        define value errors
+        """
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """
+        definin' area of square
+        """
+        return self.__size ** 2
