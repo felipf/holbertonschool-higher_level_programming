b2ed5bab8717ce3a6534c45225d6c7dba9da070f
felipf <6341@holbertonstudents.com>
2023-06-14 10:55:37 -0700

    task6 fixxxxxx

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -60,7 +60,12 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for _ in range(self.__position[1]):
+            for i in range(self.__position[1]):
                 print()
-            for _ in range(self.__size):
-                print("_" * self.__position[0] + "#" * self.__size)
+
+            for i in range(self.__size):
+                for j in range(self.__position[0]):
+                    print("_", end="")
+                for k in range(self.__size):
+                    print("#", end="")
+                print()
