fcf65a35387c0c90ff1cb8abd052af48caeb4bd0
felipf <6341@holbertonstudents.com>
2023-06-14 10:56:17 -0700

    taskk fix

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -65,7 +65,7 @@ class Square:
 
             for i in range(self.__size):
                 for j in range(self.__position[0]):
-                    print("_", end="")
+                    print(" ", end="")
                 for k in range(self.__size):
                     print("#", end="")
                 print()
