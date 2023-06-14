4a7249b77bcefd8b4e9a95ea8c1a6c2e414d6c6e
felipf <6341@holbertonstudents.com>
2023-06-14 07:07:54 -0700

    task3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+3-square.py
+author: felipf
+"""
+
+
+class Square:
+    """
+    Square defined
+    """
+
+    def __init__(self, size=0):
+        """
+        size predefined
+        """
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """
+        definin' area of square
+        """
+        return self.__size ** 2
