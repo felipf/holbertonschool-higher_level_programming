45a15d05537aaffa4cf6bb24932d166bf464f2f9
felipf <6341@holbertonstudents.com>
2023-06-18 15:04:20 -0700

    task fixed

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -4,18 +4,19 @@ task 1 or 2?
 """
 
 
-
 def matrix_divided(matrix, div):
     """
     Divide elements of a matrix
     """
-	if any(not all(isinstance(i, (int, float)) for i in n) for n in matrix):
-		raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
-	if any(len(row) != len(matrix[0]) for row in matrix):
-		raise TypeError('Each row of the matrix must have the same size')
-	if not isinstance(div, (int, float)):
-		raise TypeError('div must be a number')
-	if div == 0:
-		raise ZeroDivisionError('division by zero')
-	new_matrix = [list(map(lambda x: round(x / div, 2), n)) for n in matrix]
-	return new_matrix
+
+    if any(not all(isinstance(i, (int, float)) for i in n) for n in matrix):
+        raise TypeError('matrix must be a matrix (list of lists)'
+                        'of integers/floats')
+    if any(len(row) != len(matrix[0]) for row in matrix):
+        raise TypeError('Each row of the matrix must have the same size')
+    if not isinstance(div, (int, float)):
+        raise TypeError('div must be a number')
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+    new_matrix = [list(map(lambda x: round(x / div, 2), n)) for n in matrix]
+    return new_matrix
--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -5,16 +5,16 @@ task 3
 
 
 def print_quare(size):
-	"""
-	Print square with #
-	"""
-	if type(size) is not int:
-		raise TypeError("size must be an integer")
-	if size < 0:
-		raise ValueError("size must be >= 0")
-	if size < 0 and size < 0:
-		raise ValueError("size must be an integer")
+    """
+    Print square with #
+    """
 
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size < 0 and size < 0:
+        raise ValueError("size must be an integer")
 
-	for _ in range(size):
-		print('#' * size)
+    for _ in range(size):
+        print('#' * size)
