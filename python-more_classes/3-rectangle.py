2748e22c045e87ea7e59fe8c03e07889d8ed6f75
felipf <6341@holbertonstudents.com>
2023-06-18 15:43:07 -0700

    task 3

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+task 3
+"""
+
+
+class Rectangle():
+    """
+    creating a rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """
+        checking
+        """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        if self.__width == 0 or  self.__height == 0:
+            return ""
+        strr = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                strr += "#"
+            if i < (self.__height) - 1:
+                strr += "\n"
+        return strr
