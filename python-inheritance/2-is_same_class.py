26974c2996a372bc4835f0a4f7a21178a10e8d7d
felipf <6341@holbertonstudents.com>
2023-06-22 07:37:18 -0700

    fix

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,4 +5,7 @@ task2
 
 
 def is_same_class(obj, a_class):
+    """
+    true if is instance
+    """
     return type(obj) is a_class
--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -5,4 +5,7 @@ task3
 
 
 def is_kind_of_class(obj, a_class):
+    """
+    return true if obj is instance
+    """
     return isinstance(obj, a_class)
