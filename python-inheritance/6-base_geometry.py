2f76c6b561dd2cbb1bc94da22cc8e8eb7874779f
felipf <6341@holbertonstudents.com>
2023-06-22 07:43:31 -0700

    fixing and tasks

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+task 6
+"""
+
+
+class BaseGeometry():
+    """
+    class geo
+    """
+    def area(self):
+        """
+        exception
+        """
+        raise Exception("area() is not implemented")
