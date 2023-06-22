7740bd9e52871ca52835ed2f5733682d41d52bec
felipf <6341@holbertonstudents.com>
2023-06-22 07:24:12 -0700

    tasks

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+task 0
+"""
+
+
+def lookup(obj):
+    """
+    list of attri and meth of an obj
+    """
+    return list(dir(obj))
---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+task 1
+"""
+
+
+class MyList(list):
+    def print_sorted(self):
+        print(sorted(self))
