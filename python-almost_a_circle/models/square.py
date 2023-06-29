9ddaf8616232451b3c2118213155d0866125c3f9
Felipe Pereira Forte <felipereiraforte@gmail.com>
2023-06-29 16:17:21 -0700

    square created

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+task 10
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    square class from rectangle class
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        string repr
+        """
+        return (
+            f"[Square] ({self.id}) {self.x}/{self.y}"
+            f" - {self.width}"
+        )
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        if args and len(args) != 0:
+            try:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+                self.y = args[3]
+            except IndexError:
+                pass
+        else:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+
+    def to_dictionary(self):
+        return {
+            'id': self.id,
+            'size': self.width,
+            'x': self.x,
+            'y': self.y
+        }
