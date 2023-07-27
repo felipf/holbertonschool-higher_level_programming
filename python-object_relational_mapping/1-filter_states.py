66dd47f687bb7969516d68326753d1e422fc31cb
Felipe Pereira Forte <felipereiraforte@gmail.com>
2023-07-27 13:58:39 -0700

    task fix?

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa: """
-import MySQLdb
-from sys import argv
-
 
 if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
     """db connection"""
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     """interact cursor with database"""
     cur = db.cursor()
     """execute query"""
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
     """fetch rows as list"""
     states = cur.fetchall()
 
