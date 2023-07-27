958909fcca26d3e0caead32e6840c88a56b13658
Felipe Pereira Forte <felipereiraforte@gmail.com>
2023-07-27 13:41:45 -0700

    first task

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    """ database login """
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    """ interact cursor with database """
+    cur = db.cursor()
+    """ execute query """ 
+    cur.execute("SELECT * FROM states ORDER BY id")
+    """ fetch rows as list """
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+
+    cur.close()
+    db.close()
---
+++ python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+adding mysql to python
